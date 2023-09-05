c74c203574c75bc25e140e5790769033554fefcd
James Boulton <james@james.boulton@xtra.co.nz>
2023-09-05 12:24:09 +1200

    Threading things up.

--- docker-dashboard/main.py
+++ docker-dashboard/main.py
@@ -50,9 +50,10 @@ class LogMonitorThread(threading.Thread):
     def run(self):
         while self.running:
             try:
-                for log in self.container.logs():
-                    log_str = log.decode('utf-8').strip()
-                    self.task_sender.send_multipart([b'LOG', log_str.encode()])
+                for log in self.container.logs(stream=True, follow=True):
+                    logging.debug("%s", log)
+                    # log_str = log.decode('utf-8').strip()
+                    # self.task_sender.send_multipart([b'LOG', log_str.encode()])
             except Exception as e:
                 logging.debug(f"An error occurred: {str(e)}")
         self.task_sender.close()
